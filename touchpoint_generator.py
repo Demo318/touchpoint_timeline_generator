81d14c893bc37fc48f5a11219867caaa8c274ede
Devin Mork <devin.mork@gmail.com>
2021-03-02 15:47:58 -0600

    Validating days.

--- touchpoint_generator.py
+++ touchpoint_generator.py
@@ -3,22 +3,23 @@ This script is design to automatically generate
 a touch-point timeline for sales reps reaching
 out to prospective clients.
 """
-import datetime
-import calendar
+
+from datetime import date
+from datetime import timedelta as delta
 
 # PSUEDO CODE
 # Collect today's time & date ✔
-# Timeline saved as array inside Timeline class.
+# Timeline saved as array inside Timeline class.✔
 #   Each week is an array, [Touchpoint Action,
 #                           Day range to complete,
 #                           How many days until next touch]
 
-# Identify first day
-#   Offset from weekends
+# Identify first day as today ✔
+#   Offset from weekends ✔
 # Iterate over to make pretty looking list
+# Offer week or day shifting
+# GUI date picker?
 
-
-today = datetime.date.today()
 
 prospecting_actions = [
     ["Call/Voicemail/Email", 4, 7],
@@ -30,3 +31,15 @@ prospecting_actions = [
     ["Call/Voicemail/Email", 4, 21],
     ["Assess Account Strategy", 4, 0]
 ]
+
+
+def validate_to_next_weekday(today_var):
+    if today_var.weekday == 0:
+        today_var = today_var + delta(days=1)
+    elif today_var.weekday == 7:
+        today_var = today_var + delta(days=2)
+    else:
+        today_var
+
+
+today = date.today()
