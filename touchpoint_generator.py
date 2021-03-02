ff2d4d58698479f64e49987c9b0368adb9fdaad8
Devin Mork <devin.mork@gmail.com>
2021-03-02 14:47:15 -0600

    Add psuedocode roadmap.

--- touchpoint_generator.py
+++ touchpoint_generator.py
@@ -3,3 +3,13 @@ This script is design to automatically generate
 a touch-point timeline for sales reps reaching
 out to prospective clients.
 """
+# PSUEDO CODE
+# Collect today's time & date
+# Timeline saved as array inside Timeline class.
+#   Each week is an array, [Touchpoint Action,
+#                           Day range to complete,
+#                           How many days until next touch]
+
+# Identify first day
+#   Offset from weekends
+# Iterate over to make pretty looking list
